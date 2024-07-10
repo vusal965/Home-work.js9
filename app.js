8c70572068be7c0379fc7bad25a1309e8a17b86b
vusal965 <vusalbaxsiyev756@gmail.com>
2024-07-10 18:15:37 +0400

    Home work

---
+++ app.js
@@ -0,0 +1,28 @@
+
+// const capitalizeWords = (text1) => {
+
+// let text2 = text1.split(" ")[0][0].toUpperCase([0][0])
+// return text2
+// };
+// console.log(capitalizeWords("salam necesen"));
+
+
+
+
+const numCounter = (num) => {
+return num.length
+
+}
+console.log(numCounter("485963214566"));
+
+
+// const isValidNumber = (number) => {
+//  let num2 = number.length
+
+//     if(num2.length == 11)   {
+//         return true
+//     }else{
+//         return false
+//     }
+//     }
+//     console.log(isValidNumber(+71111111111));
